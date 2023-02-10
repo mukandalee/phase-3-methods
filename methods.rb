a6fda9a284cce3ca8c7af353be2c7762646b0fa5
mukandalee <leonardmukanda254@gmail.com>
2023-02-10 06:07:28 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+
+def  greetProgrammer() 
+  puts "Hello, programmer!"
+end 
+puts greetProgrammer
+
+def  greet(name) 
+  puts"Hello, #{name}!"
+end
+puts greet("leo")
+
+
+def  greetWithDefault(name = "programmer") 
+  puts "Hello, #{name}!"
+end
+puts greetWithDefault
+
+def  add(num1, num2) 
+  return num1 + num2;
+end
+puts add 5,8
+
+def  halve(number) 
+  if number.class == "Number" 
+    nil
+  end
+  return number / 2;
+  end 
+  puts halve 8
